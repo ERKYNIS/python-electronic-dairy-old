b5550e4dfffa5bc07b682dfd1085f62455192334
Nikita Belkin <76586422+ERKYNIS@users.noreply.github.com>
2023-03-24 18:09:04 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def main():
 
 @app.route('/account')
 def index():
-    return render_template('account.html', title="Аккаунт")
+    return render_template('login.html', title="Аккаунт")
 
 
 if __name__ == '__main__':
